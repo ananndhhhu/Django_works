573fa2a65cacb12c08ef76bdfc1b717392bbace7
MeAnanndhhhu <anandhu192532@gmail.com>
2026-05-18 14:36:01 +0530

    Delete demopro directory

--- demopro/app1/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from django.shortcuts import render
-
-# Create your views here.
-def home(request):
-    return HttpResponse('Welcome To Django')
-
-def index(request):
-    return HttpResponse('Hallo')
